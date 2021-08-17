ca0fc1b4938998e0b167b4e73805dd2a23be1c94
Cyrine Maaloul <you@example.com>
2021-08-17 12:02:28 +0100

    last update back

--- src/main/java/com/health/talan/entities/Community.java
+++ src/main/java/com/health/talan/entities/Community.java
@@ -4,17 +4,7 @@ import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name= "community")
